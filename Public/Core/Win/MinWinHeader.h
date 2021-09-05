53cb0597e73e9382da46e5017261d68d86648b1d
Marcos Sánchez <marcossanchez405@gmail.com>
2021-09-06 00:06:46 +0200

    Some changes were missing

--- Public/Core/Win/MinWinHeader.h
+++ Public/Core/Win/MinWinHeader.h
@@ -11,9 +11,18 @@
 #if true
 #define DECLSPEC_IMPORT __declspec(dllimport)
 #define RPCRTAPI DECLSPEC_IMPORT
+#define WINUSERAPI DECLSPEC_IMPORT
+#define WINBASEAPI DECLSPEC_IMPORT
+
+#define WINAPI __stdcall
+
+#define DECLARE_HANDLE(name) struct name##__{int unused;}; typedef struct name##__ *name
+
 typedef long RPC_STATUS;
 #	define  RPC_ENTRY __stdcall
 #define __RPC_FAR
+#define far
+#define FAR far
 
 typedef struct _GUID {
     unsigned long  Data1;
@@ -23,8 +32,42 @@ typedef struct _GUID {
 } GUID;
 
 typedef GUID UUID;
+#define CONST               const
+typedef int                 INT;
+typedef int                 BOOL;
+typedef __int64 INT_PTR, *PINT_PTR;
+typedef unsigned int        UINT;
+typedef unsigned int        *PUINT;
+
+typedef char CHAR;
+typedef CHAR *PCHAR, *LPCH, *PCH;
+typedef CONST CHAR *LPCCH, *PCCH;
+typedef CHAR *NPSTR, *LPSTR, *PSTR;
+typedef CONST CHAR *LPCSTR, *PCSTR;
+
+typedef wchar_t WCHAR;
+typedef WCHAR *PWCHAR, *LPWCH, *PWCH;
+typedef CONST WCHAR *LPCWCH, *PCWCH;
+typedef WCHAR *NWPSTR, *LPWSTR, *PWSTR; 
+typedef CONST WCHAR *LPCWSTR, *PCWSTR;
+typedef CONST WCHAR *LPCWCHAR, *PCWCHAR;
 
 
+
+typedef INT_PTR (FAR WINAPI *FARPROC)();
+
+#define MB_ICONHAND                 0x00000010L
+#define MB_ICONERROR                MB_ICONHAND
+#define MB_ABORTRETRYIGNORE         0x00000002L
+#define MB_OK                       0x00000000L
+#define MB_TASKMODAL                0x00002000L
+#define MB_TOPMOST                  0x00040000L
+#define IDRETRY             4
+#define IDIGNORE            5
+
+DECLARE_HANDLE(HWND);
+DECLARE_HANDLE(HINSTANCE);
+typedef HINSTANCE HMODULE;
 
 RPCRTAPI
 RPC_STATUS
@@ -32,6 +75,39 @@ RPC_ENTRY
 UuidCreate(
     UUID __RPC_FAR* Uuid
 );
+
+
+WINUSERAPI
+int
+WINAPI
+MessageBoxA(
+    HWND hWnd,
+    LPCSTR lpText,
+    LPCSTR lpCaption,
+    UINT uType);
+
+WINBASEAPI
+FARPROC
+WINAPI
+GetProcAddress(
+    HMODULE hModule,
+    LPCSTR lpProcName
+    );
+
+WINBASEAPI
+HMODULE
+WINAPI
+LoadLibraryW(
+    LPCWSTR lpLibFileName
+    );
+
+WINBASEAPI
+BOOL
+WINAPI
+FreeLibrary(
+    HMODULE hLibModule
+    );
+
 
 #else
 #include <Windows.h>
